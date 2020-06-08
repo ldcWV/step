221f3fd917320565505f2f9d2a324e0948c297ac
Lawrence Chen <Ldorianchen@gmail.com>
2020-06-08 16:42:58 +0000

    Add login and logout functionality

--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -35,7 +35,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/comment-data")
-public class DataServlet extends HttpServlet {
+public class CommentsServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
